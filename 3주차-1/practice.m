707a6587c1c24f44448647221940559431cf59cb
kenter7317 <kenter7317@gmail.com>
2025-03-19 11:30:55 +0900

    first commit

---
+++ 3주차-1/practice.m
@@ -0,0 +1,9 @@
+
+a = 100;
+if a < 20
+    fprintf('%d less than 20\n', a);
+elseif a < 40
+    fprintf('%d bigger than or equals 40\n', a);
+else
+    fprintf('%d bigger than or equals 20\n', a);
+end
